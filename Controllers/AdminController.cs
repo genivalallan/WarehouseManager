986eade1e6780cd17f59300197461a2d8ce9ba80
Allan Mariano <genivalallan@hotmail.com.br>
2021-03-25 00:37:20 -0300

    Create Admin controller class and move DbInit endpoint from Home to Admin controller

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+using WarehouseManager.Models;
+
+namespace WarehouseManager.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IWMRepository repository;
+
+        public AdminController(IWMRepository repo) => repository = repo;
+
+        public IActionResult DbInit() => View(repository.DbInit());
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+using WarehouseManager.Models;
+
+namespace WarehouseManager.Controllers
+{
+    public class HomeController : Controller
+    {
+    }
+}
