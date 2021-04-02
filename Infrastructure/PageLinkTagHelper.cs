7d9a5fe141a0683e3b35a35cc802b45d67fc3915
Allan Mariano <genivalallan@hotmail.com.br>
2021-04-01 21:21:53 -0300

    Create Paging Tag Helper

---
+++ Infrastructure/PageLinkTagHelper.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+using WarehouseManager.Models.ViewModels;
+
+namespace WarehouseManager.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PageLinkTagHelper : TagHelper
+    {
+        private readonly IUrlHelperFactory urlHelperFactory;
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+        public PagingInfo PageModel { get; set; }
+        public string PageAction { get; set; }
+
+
+        public PageLinkTagHelper(IUrlHelperFactory helper) => urlHelperFactory = helper;
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            TagBuilder result = new TagBuilder("div");
+
+            for (int i = 1; i < PageModel.TotalPages; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+
+                tag.Attributes["href"] = urlHelper.Action(PageAction, new { page = i });
+                tag.InnerHtml.Append(i.ToString());
+
+                result.InnerHtml.AppendHtml(tag);
+            }
+
+            output.Content.AppendHtml(result.InnerHtml);
+        }
+    }
+}
---
+++ Models/ViewModels/PagingInfo.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace WarehouseManager.Models.ViewModels
+{
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int Page { get; set; }
+        public int TotalPages =>
+            (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
+    }
+}
