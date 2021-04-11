d0f23ad2fcd9b0abc9d8b4ed21eded4aaddbcfc0
Allan Mariano <genivalallan@hotmail.com.br>
2021-04-10 22:59:13 -0300

    Add navigation buttons

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -10,6 +10,6 @@ namespace WarehouseManager.Controllers
 
         public AdminController(IWMRepository repo) => repository = repo;
 
-        public IActionResult DbInit(string sample) => View(repository.DbInit(sample == "true"));
+        public IActionResult DbInit() => View(repository.DbInit(HttpContext.Request.Query["sample"] == "true"));
     }
 }
