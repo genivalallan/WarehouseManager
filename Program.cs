a0e32c114380e61adb7afbd3cb8e70828c1a80d3
Allan Mariano <genivalallan@hotmail.com.br>
2021-04-24 22:34:49 -0300

    Application address is set in /Properties/launchSettings.json

--- Program.cs
+++ Program.cs
@@ -21,7 +21,6 @@ namespace WarehouseManager
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://0.0.0.0:5000");
                 });
     }
 }
