40e1791997ffa7bb9dfa00c4d0cf2166fc62bd0b
Allan Mariano <genivalallan@hotmail.com.br>
2021-07-04 15:47:29 -0300

    Apply filter to StocksController's List action

--- Controllers/StocksController.cs
+++ Controllers/StocksController.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
 
 using WarehouseManager.Models;
 using WarehouseManager.Models.ViewModels;
@@ -15,43 +16,109 @@ namespace WarehouseManager.Controllers
     public class StocksController : Controller
     {
         private readonly IWMRepository repository;
-        private int itemsPerPage = 5;
 
         public StocksController(IWMRepository repo) => repository = repo;
 
         [HttpGet]
-        public IActionResult List([FromQuery]int page = 1)
-        {
-            IEnumerable<Stock> stocks = null;
-            PagingInfo pagingInfo = new PagingInfo(
-                repository.Stocks.Count(), itemsPerPage, page);
-            
-            if (pagingInfo.TotalItems != 0)
+        public IActionResult List(
+            string orderby,
+            string order,
+            string searchby,
+            string search,
+            int page = 1
+        ){
+            List<Stock> stocks = null;
+            PagingInfo paging = new PagingInfo();
+            ListFilter filter = new ListFilter();
+
+            if (repository.Stocks.Count() != 0)
             {
-                if (pagingInfo.Page < 1 ||
-                    pagingInfo.Page > pagingInfo.TotalPages)
+                orderby = String.IsNullOrWhiteSpace(orderby) ? "id" : orderby.Trim();
+                order = String.IsNullOrWhiteSpace(order) ? "asc" : order.Trim();
+
+                if ((orderby != "id" && orderby != "product" && orderby != "owner" && orderby != "balance") ||
+                    (order != "asc" && order != "desc"))
                 {
-                    return NotFound();
+                    return RedirectToAction();
                 }
 
-                stocks = repository.Stocks
-                    .OrderBy(s => s.ID)
-                    .Skip((pagingInfo.Page - 1) * pagingInfo.ItemsPerPage)
-                    .Take(pagingInfo.ItemsPerPage)
-                    .Include(s => s.Product)
-                    .Include(s => s.Owner)
-                    .AsNoTracking();
+                filter.Order = order;
+                filter.OrderBy = orderby;
+
+                string filterQuery = "SELECT stock.*, product.name AS product, client.name AS owner FROM stock"
+                    + " INNER JOIN product ON stock.product_id = product.id"
+                    + " INNER JOIN client ON stock.client_id = client.id";
+
+                MySqlParameter p0 = null;
+                if (!String.IsNullOrWhiteSpace(search = search?.Trim()))
+                {
+                    searchby = searchby?.Trim();
+                    if (searchby != "product" && searchby != "owner")
+                    {
+                        return RedirectToAction();
+                    }
+                    else
+                    {
+                        filter.SearchBy = searchby;
+                        filter.Search = search;
+                        filterQuery += $" WHERE {(searchby == "owner" ? "client" : "product")}.name LIKE @search";
+                        p0 = new MySqlParameter("@search", $"%{filter.Search}%");
+                    }
+                }
+
+                filterQuery += $" ORDER BY {orderby} {filter.Order}";
+
+                paging.TotalItems = repository.DbContext().Stocks
+                    .FromSqlRaw(filterQuery, p0).Count();
+
+                paging.Page = (page <= 1) ? 1 : (page >= paging.TotalPages ? paging.TotalPages : page);
+
+                stocks = repository.DbContext().Stocks
+                    .FromSqlRaw(filterQuery, p0)
+                    .Skip((paging.Page - 1) * paging.ItemsPerPage)
+                    .Take(paging.ItemsPerPage)
+                    .AsNoTracking()
+                    .ToList();
+
+                for (int i = 0; i < stocks.Count; i++)
+                {
+                    stocks[i].Product = repository.Products.Single(p => p.ID == stocks[i].ProductID);
+                    stocks[i].Owner = repository.Clients.Single(p => p.ID == stocks[i].ClientID);
+                }
             }
 
-            ViewData["Title"] = "Lista de Estoques";
-            ViewData["Entity"] = "Estoques";
-            ViewData["Controller"] = "stocks";
-            ViewData["Action"] = "list";
             return View(new ListViewModel
             {
                 JsonItems = JsonSerializer.Serialize(stocks),
-                PagingInfo = pagingInfo
+                PagingInfo = paging,
+                ListFilter = filter
             });
+        }
+
+        [HttpPost]
+        public IActionResult List(
+            [FromForm]string orderby,
+            [FromForm]string order,
+            [FromForm]string searchby,
+            [FromForm]string search,
+            int _,
+            [FromForm]int page = 1
+        ){
+            orderby ??= "";
+            order ??= "";
+            searchby ??= "";
+            search = search?.Trim();
+
+            object query = new
+            {
+                page = (page == 1) ? null : page.ToString(),
+                orderby = (orderby == "id") ? null : orderby.Trim(),
+                order = (order == "asc") ? null : order.Trim(),
+                searchby = String.IsNullOrEmpty(search) ? null : searchby,
+                search
+            };
+
+            return RedirectToAction("List", "Stocks", query);
         }
 
         [HttpGet]
